26be97f1832de3c48816232b96c8fbe9338ddcc0
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2025-05-14 16:17:29 +0300

    Set up API routes and Sanctum token authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,25 +9,27 @@ use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Validate CSRF Tokens for frontend (excluding certain routes)
         $middleware->validateCsrfTokens(except: [
-           '/register',
-          '/login',
+            '/register',
+            '/login',
             '/products',
             '/products/{id}',
         ]);
+        
         // Add Sanctum's middleware for stateful requests
         $middleware->alias([
             'sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'abilities' => CheckAbilities::class,  // For checking all abilities
-            'ability' => CheckForAnyAbility::class,  // For checking at least one ability
+            'abilities' => CheckAbilities::class,
+            'ability' => CheckForAnyAbility::class,
         ]);
-        
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // Handle exceptions here
     })
     ->create();
